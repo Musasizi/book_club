814dc5c8e467c853125c82322c836434c04d98e6
Musasizi Kenneth <kennymusasizi@hotmail.com>
2026-03-19 08:39:52 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Content from "./components/Content";
+import Title from "./components/Title";
+
+
+function App() {
+
+    return (
+        <>
+            <Title text={"BSIT"} />
+
+            <Title text={"MEDICINE"} />
+
+            <Title text={"ENGINEERING"} />
+
+            <Content />
+        </>
+    );
+
+
+}
+
+export default App;
